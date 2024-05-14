6fbffdc00909c2c6049e1b9332443016acd50a13
Carl Osterwisch <costerwi@gmail.com>
2024-05-14 13:55:28 -0400

    Add QAT and replace QPP with QPT

--- licenseLevel_plugin.py
+++ licenseLevel_plugin.py
@@ -15,8 +15,9 @@ __version__ = "1.0.1"
 
 # List of Abaqus features to report. Note: license trigram must be the first word
 features = (
-    'QPP tokens shared by Abaqus CAE and Abaqus solver\tAbaqus "Portfolio" licenses',
-    'QXT solver tokens for Abaqus, fe-safe, Tosca, Isight\tAbaqus "Extended" solver licenses',
+    'QAT solver tokens for Abaqus only\tAbaqus "Analysis" licenses',
+    'QPT tokens for either Abaqus CAE or Abaqus solver\tAbaqus "Portfolio" licenses',
+    'QXT solver tokens for Abaqus, fe-safe, Tosca, Isight\tSimulia "Extended" solver licenses',
     'SRU solver tokens for above plus CST, Simpack, PowerFLOW\tSimulia universal "SimUnit" solver licenses',
     'SUN solver credits for above plus CST, Simpack, PowerFLOW\tSimulia pre-paid consumable "SimUnit" solver licenses',
     'QAX tokens for Abaqus CAE, fe-safe GUI, Isight Gateway\tAbaqus "Extended" graphical user interface licenses',
@@ -50,8 +51,8 @@ class licenseLevelDB(AFXDataDialog):
             self.progress[trigram] = AFXProgressBar(h,
                 opts=LAYOUT_FIX_WIDTH|LAYOUT_FIX_HEIGHT|AFXPROGRESSBAR_ITERATOR, 
                 w=200, h=22)
-            if 'QPP' == trigram:
-                h.hide()  # QPP is deprecated; hide unless it is available
+            if trigram in ('QAT', 'QPT'):
+                h.hide()  # hide deprecated tokens unless they are available on server
 
         p = FXGroupBox(self, 'Usage details', opts=FRAME_GROOVE|LAYOUT_FILL_X|LAYOUT_FILL_Y)
         self.text = FXText(p,
