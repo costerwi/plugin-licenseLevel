a8648ad5a85c05f4bb50725bec41c05fec1de3a3
Carl Osterwisch <costerwi@gmail.com>
2024-09-08 07:52:49 -0400

    Use UsageLine class to sum licenses from the same jobID

--- licenseLevel.py
+++ licenseLevel.py
@@ -10,11 +10,44 @@ https://github.com/costerwi/plugin-licenseLevel
 """
 
 from __future__ import print_function
+import re
 import sys
+
+class UsageLine:
+    "Convenience class for working with license usage strings"
+
+    def __init__(self, line):
+        self.line = str(line).strip()
+
+    def jobId(self):
+        "Extract unique job Id from raw usage line"
+        m = re.search('\(.+\)', self.line)
+        if m:
+            return m.group(0)
+        return ''
+
+    def getLicenses(self):
+        "Return int number of licenses"
+        m = re.search('(\d+) licenses', self.line)
+        if m:
+            return int(m.group(1))
+        return 0
+
+    def setLicenses(self, n):
+        "Set number of licenses"
+        self.line = re.sub('\d+ license', '{} license'.format(n), self.line)
+
+    def addLicenses(self, other):
+        "Add licenses from another UsageLine"
+        self.setLicenses(self.getLicenses() + other.getLicenses())
+
+    def __str__(self):
+        "Return abreviated string with jobId removed"
+        return re.sub('\(.+, ', '', self.line)
+
 
 def dslsstat():
     "Collect current license usage data using dslsstat"
-    import re
     import subprocess
     from subprocess import Popen, PIPE
     import sys
@@ -49,8 +82,19 @@ def dslsstat():
             feature.update(row)
         elif feature and 'using' in line:
             # collect detailed usage data from lines following feature
-            usage = feature.setdefault('usage', [])
-            usage.append(re.sub('\(.+, ', '', line.strip()))
+            usage = feature.setdefault('usage', {})
+            usageLine = UsageLine(line)
+            jobId = usageLine.jobId()
+            if jobId in usage:
+                usage[jobId][1].addLicenses(usageLine)
+            else:
+                usage[jobId] = (len(usage), usageLine)
+    for trigram, data in summary.items():
+        # Convert usage dict to ordered list of strings
+        if not 'usage' in data:
+            continue
+        usage = data.get('usage')
+        data['usage'] = [str(usageLine) for order, usageLine in sorted(usage.values())]
     return summary
 
 def printSummary(trigrams=[]):
