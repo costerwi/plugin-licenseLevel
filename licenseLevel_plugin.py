727f1a709d100ca023178ef867e944c67f9a4f5d
Carl Osterwisch <costerwi@gmail.com>
2025-02-13 14:04:11 -0500

    Version bump 1.3.0

--- licenseLevel_plugin.py
+++ licenseLevel_plugin.py
@@ -11,7 +11,7 @@ import abaqusGui
 from kernelAccess import session
 import sys
 
-__version__ = "1.2.1"
+__version__ = "1.3.0"
 
 # List of Abaqus features to report. Note: license trigram must be the first word
 features = (
